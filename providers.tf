df9d1981a485b35bf04cdc3aa77efef441820603
falconstor <110676899+falconstor-code@users.noreply.github.com>
2024-04-19 15:02:47 -0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -6,3 +6,15 @@ terraform {
     }
   }
 }
+
+locals {
+  pvs_info = split(":", var.crn)
+  location = local.pvs_info[5]
+  region   = can(regex("-", local.location)) ? (can(regex("-[0-9]+$", local.location)) ? replace(local.location, regex("-[0-9]+$", local.location), "") : local.location) : (can(regex("[0-9]+$", local.location)) ? replace(local.location, regex("[0-9]+$", local.location), "") : local.location)
+  pid      = local.pvs_info[7]
+}
+
+provider "ibm" {
+  region = local.region
+  zone   = local.location
+}
